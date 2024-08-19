31a8112938c63e65c1e96489863eb97d7b5b74db
Radish <53844660+radixsh@users.noreply.github.com>
2024-08-18 18:03:23 -0700

    Break up into logical functions

--- analyze.py
+++ analyze.py
@@ -5,6 +5,88 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 from datetime import datetime
 
+def graph_data(data):
+    print('Data to be graphed:')
+    pprint(data)
+
+    plt.xlabel('Timestamp')
+    plt.ylabel('Weight')
+
+    timespan = [x[0] for x in data]
+    weights = [x[1] for x in data]
+
+    # https://matplotlib.org/stable/api/_as_gen/matplotlib.colors.Normalize.html#matplotlib.colors.Normalize
+    reps = [x[2] for x in data]
+    norm = mpl.colors.Normalize(vmin=0, vmax=1, clip=False)
+
+    plt.scatter(timespan, weights, c=norm(reps))    #label="Bench press", c=norm(reps))
+
+    # plt.legend()#bbox_to_anchor=(1,1), loc="upper right")
+    plt.savefig("bench_press.png")#, bbox_inches="tight")
+    plt.show()
+
+def show_all_tables(conn):
+    tables = [#'exercise_table', 
+              'workout_table',
+              'routine_set_table', 'routine_set_group_table',
+              'workout_set_table', 'workout_set_group_table'
+              ]
+    for table in tables:
+        print(table)
+        count = 0
+        for row in conn.execute(f"SELECT * FROM {table}"):
+            count += 1
+            if count >= 10:
+                break
+            print(row)
+        print()
+    print('\n')
+
+def find_exercise_data(exercise_name, conn):
+    res = conn.execute(f"SELECT * FROM exercise_table WHERE name == '{exercise_name}'")
+    exercise_id = res.fetchone()['exerciseId']
+
+    ids = []
+    print(f'Seeking bench_press (ID={exercise_id}) data in workout_set_group_table')
+    for row in conn.execute(f"SELECT * FROM workout_set_group_table"):
+        if row['exerciseId'] == exercise_id:
+            ids.append(row['id'])
+            pprint(row)
+    print(f'ids: {ids}')
+
+    data = []
+    print('\nworkout_set_table')
+    index = 0
+    for row in conn.execute(f"SELECT * FROM workout_set_table"):
+        index += 1
+        
+        # groupId identifies a unique combination of workout ID and exercise ID
+        if not (row['groupId'] in ids and row['complete']):
+            continue 
+        print(f'workout_set_table row: {row}')
+
+        # Find timestamp
+        j = 1       # Account for index incrementing at start of 'for' loop
+        for row in conn.execute(f"SELECT * FROM routine_set_table"):
+            if j == index:
+                group_id = row['groupId']
+                print(f'routine_set_table row: {row}')
+                break
+            j += 1
+
+        cmd = f"SELECT * FROM routine_set_group_table WHERE id == '{group_id}'"
+        workout_id = conn.execute(cmd).fetchone()['routineId']
+        
+        cmd = f"SELECT * FROM workout_table WHERE routineId == '{workout_id}'"
+        timestamp = conn.execute(cmd).fetchone()['startTime']
+        
+        # https://stackoverflow.com/questions/3682748/converting-unix-timestamp-string-to-readable-date
+        timestamp = datetime.fromtimestamp(timestamp / 1e3)
+
+        data.append((timestamp, row['weight'], row['reps']))
+
+    return data
+
 def dict_factory(cursor, row):
     # https://stackoverflow.com/questions/3300464/how-can-i-get-dict-from-sqlite-query
     # https://docs.python.org/3/library/sqlite3.html#sqlite3-howto-row-factory
@@ -15,92 +97,18 @@ def dict_factory(cursor, row):
 def main():
     try:
         # https://docs.python.org/3/library/sqlite3.html#tutorial
-        con = sqlite3.connect(sys.argv[1])
-        con.row_factory = dict_factory
+        conn = sqlite3.connect(sys.argv[1])
+        conn.row_factory = dict_factory
     except:
         print('Usage: ./analyze.py gymroutines.db')
         sys.exit(0)
     
-    exercise_name = 'bench press'
-    res = con.execute(f"SELECT * FROM exercise_table WHERE name == '{exercise_name}'")
-    exercise_id = res.fetchone()['exerciseId']
-
-    tables = [#'exercise_table', 
-              #'workout_table',
-              #'routine_set_table', 'routine_set_group_table',
-              #'workout_set_table', 'workout_set_group_table'
-              ]
-    for table in tables:
-        print(table)
-        count = 0
-        for row in con.execute(f"SELECT * FROM {table}"):
-            count += 1
-            if count >= 10:
-                break
-            print(row)
-        print()
-
-    print('\n')
-
+    show_all_tables(conn)
 
     # Find data pertaining to 'exercise_name' across all workouts
-    ids = []
-    print(f'Seeking bench_press (ID={exercise_id}) data in workout_set_group_table')
-    for row in con.execute(f"SELECT * FROM workout_set_group_table"):
-        if row['exerciseId'] == exercise_id:
-            ids.append(row['id'])
-            pprint(row)
-    print(f'ids: {ids}')
+    data = find_exercise_data('bench press', conn)
 
-    data = []
-    print('\nworkout_set_table')
-    index = 0
-    for row in con.execute(f"SELECT * FROM workout_set_table"):
-        index += 1
-        
-        # groupId identifies a unique combination of workout ID and exercise ID
-        if not (row['groupId'] in ids and row['complete']):
-            continue 
-        print(f'workout_set_table row: {row}')
-
-        # Find timestamp
-        j = 1       # Account for index incrementing at start of 'for' loop
-        for row in con.execute(f"SELECT * FROM routine_set_table"):
-            if j == index:
-                group_id = row['groupId']
-                print(f'routine_set_table row: {row}')
-                break
-            j += 1
-        
-        cmd = f"SELECT * FROM routine_set_group_table WHERE id == '{group_id}'"
-        workout_id = con.execute(cmd).fetchone()['routineId']
-        
-        cmd = f"SELECT * FROM workout_table WHERE routineId == '{workout_id}'"
-        timestamp = con.execute(cmd).fetchone()['startTime']
-
-        # Append a tuple to our data 
-        data.append((datetime.fromtimestamp(timestamp / 1e3), row['weight'], row['reps']))
-        print()
-
-    print(f'data: {data}')
-
-    # Graph our data 
-    plt.xlabel('Timestamp')
-    plt.ylabel('Weight')
-
-    # https://stackoverflow.com/questions/3682748/converting-unix-timestamp-string-to-readable-date
-    # timespan = [datetime.fromtimestamp(x[0] / 1e3) for x in data]
-    timespan = [x[0] for x in data]
-    weights = [x[1] for x in data]
-    reps = [x[2] for x in data]
-    norm = mpl.colors.Normalize(vmin=-1, vmax=1, clip=False)
-    plt.scatter(timespan, weights, label="alsdkfj", c=norm(reps))
-    # plt.plot(timespan, weights)
-
-    # plt.legend(bbox_to_anchor=(1,1), loc="upper right")
-    plt.savefig("bench_press.png")#, bbox_inches="tight")
-    plt.show()
-
+    graph_data(data)
 
 if __name__ == "__main__":
     main()
